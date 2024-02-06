8a8914429326ed695e3aa58537ee2bdf3541fe2a
vasu63devan <54571600+vasu63devan@users.noreply.github.com>
2024-02-06 22:29:27 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://media.giphy.com/media/ceeFbVxiZzMBi/giphy.gif" alt="logo" />
+        <h2>I love Coffee!</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
